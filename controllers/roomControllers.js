e75b04d298f57b8320af71750a58ebed7dbf12e2
MAbdurahman <mdbdrrhm@gmail.com>
2021-07-12 04:18:30 -0500

    Add create new room to api routes

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,8 +1,32 @@
+import Room from '../models/roomModel';
 
+/*===============================================================
+         Get All Rooms => (GET)/api/rooms
+==================================================================*/
+const allRooms = (req, res) => {
+	res.status(200).json({
+		success: true,
+		message: 'All Rooms!',
+	});
+};
 
-export const allRooms = (req, res) => {
-   res.status(200).json({
-      success: true,
-      message: 'All Rooms!'
-   })
-}
+/*===============================================================
+            Create A New Room => (post)/api/rooms
+==================================================================*/
+const newRoom = async (req, res) => {
+	try {
+		const room = await Room.create(req.body);
+
+		res.status(200).json({
+			success: true,
+			room,
+		});
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
+
+export { allRooms, newRoom };
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,11 +1,13 @@
 import nc from "next-connect";
 import connectDatabase from "../../../config/connectDatabase";
-import {allRooms} from "../../../controllers/roomControllers";
+import {allRooms, newRoom} from "../../../controllers/roomControllers";
 
 const handler = nc();
 
 connectDatabase();
 
 handler.get(allRooms);
+
+handler.post(newRoom);
 
 export default handler;
