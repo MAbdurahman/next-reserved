5231e3daaf4617475e1bb97989bb02d23eea47d0
MAbdurahman <mdbdrrhm@gmail.com>
2021-07-12 11:30:23 -0500

    Add catchAsyncErrors middleware

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,10 +1,11 @@
 import Room from '../models/roomModel';
 import ErrorHandler from '../utils/errorHandler';
+import catchAsyncErrors from '../middlewares/catchAsyncErrors';
 
 /*===============================================================
          Get All Rooms => (GET)/api/rooms
 ==================================================================*/
-const allRooms = async (req, res, next) => {
+const allRooms = catchAsyncErrors(async (req, res, next) => {
 	try {
 		const rooms = await Room.find({});
 
@@ -19,12 +20,12 @@ const allRooms = async (req, res, next) => {
 			error: error.message,
 		});
 	}
-};
+});
 
 /*===============================================================
             Create A New Room => (post)/api/rooms
 ==================================================================*/
-const newRoom = async (req, res) => {
+const newRoom = catchAsyncErrors(async (req, res) => {
 	try {
 		const room = await Room.create(req.body);
 
@@ -38,32 +39,32 @@ const newRoom = async (req, res) => {
 			error: error.message,
 		});
 	}
-};
+});
 
 /*===============================================================
             Get Room Details => (GET)/api/rooms/:id
 ==================================================================*/
-const getSingleRoom = async (req, res, next) => {
+const getSingleRoom = catchAsyncErrors(async (req, res, next) => {
 	const room = await Room.findById(req.query.id);
 
 	if (!room) {
-		return next(new ErrorHandler('A room was not found with this ID!', 404));
+		return next(new ErrorHandler('A room with this ID was not found!', 404));
 	}
 
 	res.status(200).json({
 		success: true,
 		room,
 	});
-};
+});
 
 /*===============================================================
             Update Room Details => (PUT)/api/rooms/:id
 ==================================================================*/
-const updateRoom = async (req, res, next) => {
+const updateRoom = catchAsyncErrors(async (req, res, next) => {
 	const room = await Room.findById(req.query.id);
 
 	if (!room) {
-		return next(new ErrorHandler('A room was not found with this ID!', 404));
+		return next(new ErrorHandler('A room with this ID was not found!', 404));
 	}
 
 	const updateRoom = await Room.findByIdAndUpdate(req.query.id, req.body, {
@@ -76,16 +77,16 @@ const updateRoom = async (req, res, next) => {
 		success: true,
 		updateRoom,
 	});
-};
+});
 
 /*===============================================================
             Delete Room => (DELETE)/api/rooms/:id
 ==================================================================*/
-const deleteRoom = async (req, res) => {
+const deleteRoom = catchAsyncErrors(async (req, res) => {
 	const room = await Room.findById(req.query.id);
 
 	if (!room) {
-		return next(new ErrorHandler('A room was not found with this ID!', 404));
+		return next(new ErrorHandler('A room with this ID was not found!', 404));
 	}
 
 	await room.remove();
@@ -94,6 +95,6 @@ const deleteRoom = async (req, res) => {
 		success: true,
 		message: 'Room has been deleted!',
 	});
-};
+});
 
 export { allRooms, newRoom, getSingleRoom, updateRoom, deleteRoom };
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,8 +1,9 @@
 import nc from "next-connect";
 import connectDatabase from "../../../config/connectDatabase";
 import {allRooms, newRoom} from "../../../controllers/roomControllers";
+import onError from '../../../middlewares/errors';
 
-const handler = nc();
+const handler = nc({ onError });
 
 connectDatabase();
 
