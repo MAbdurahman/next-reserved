d951a0fde905da1fc1700af1e63d298eacad6607
MAbdurahman <mdbdrrhm@gmail.com>
2021-07-15 14:42:14 -0500

    Modify make correction for Image tag to function properly

--- components/room/RoomItem.jsx
+++ components/room/RoomItem.jsx
@@ -6,11 +6,11 @@ export default function RoomItem({ room }) {
    return (
 		<div className='col-sm-12 col-md-6 col-lg-3 my-3'>
 			<div className='card p-2'>
-				<img
+				<Image
 					className='card-img-top mx-auto'
 					src={room.images[0].url}
 					height={170}
-					width=''
+					width={100}
 				/>
 				<div className='card-body d-flex flex-column'>
 					<h5 className='card-title'>
