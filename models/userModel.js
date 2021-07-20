de307607b914bdd067c4c21b7da57c06c3c23d7a
MAbdurahman <mdbdrrhm@gmail.com>
2021-07-20 04:41:45 -0500

    Add next-auth and dynamic configuration

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import validator from 'validator';
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs';
 
 //**************** variables ****************//
 const name_pattern = /^([a-zA-Z]{2,}\s[a-zA-z]{1,}'?-?[a-zA-Z]{1,}\s?([a-zA-Z]{1,})?)/i;
@@ -31,13 +31,16 @@ const userSchema = mongoose.Schema({
 		trim: true,
 		required: [true, 'Password is required!'],
 		minLength: [8, 'Password must be at least 8 characters!'],
-		match: [password_pattern, 'Password must have at least one of the following: lowercase, uppercase, number, and special character!'],
+		match: [
+			password_pattern,
+			'Password must have at least one of the following: lowercase, uppercase, number, and special character!',
+		],
 		select: false,
 	},
 	avatar: {
 		public_id: {
 			type: String,
-			required: true
+			required: true,
 		},
 		url: {
 			type: String,
@@ -53,33 +56,27 @@ const userSchema = mongoose.Schema({
 		type: Date,
 		default: Date.now,
 	},
-	isVerified: {
-		type: Boolean,
-		default: false,
-	},
 	resetPasswordToken: String,
 	resetPasswordExpire: Date,
 });
 
 //**************** Encrypting password before saving ****************//
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
 	if (!this.isModified('password')) {
 		next();
 	}
 
 	this.password = await bcrypt.hash(this.password, 10);
-
 });
 
+//**************** Compare User Password ****************//
+userSchema.methods.comparePassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
 //**************** variables ****************//
 //**************** variables ****************//
 //**************** variables ****************//
 //**************** variables ****************//
 //**************** variables ****************//
-//**************** variables ****************//
-
-
-
-
 
 export default mongoose.models.User || mongoose.model('User', userSchema);
