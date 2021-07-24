64cbfc9cc7acb16e5f3a2544d970cc46bda74fad
MAbdurahman <mdbdrrhm@gmail.com>
2021-07-24 16:39:45 -0500

    Add getServerSideProps method to protect routes on client side

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,28 @@
-import Login from "./../components/auth/Login";
-import Layout from "./../components/layout/Layout";
+import Login from './../components/auth/Login';
+import Layout from './../components/layout/Layout';
+import { getSession } from 'next-auth/client';
 
 export default function LoginPage() {
-   return (
-      <Layout title='e-Reserve | Login'>
-         <Login />
-      </Layout>
-   )
+	return (
+		<Layout title='e-Reserve | Login'>
+			<Login />
+		</Layout>
+	);
+}
+
+export async function getServerSideProps(context) {
+	const session = await getSession({ req: context.req });
+
+	if (session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
 }
--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,28 @@
-import Register from "./../components/auth/Register";
-import Layout from "./../components/layout/Layout";
-import { getSession } from "next-auth/client";
+import Register from './../components/auth/Register';
+import Layout from './../components/layout/Layout';
+import { getSession } from 'next-auth/client';
 
 export default function RegisterPage() {
-   return (
+	return (
 		<Layout title='e-Reserve | Register'>
 			<Register />
 		</Layout>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const session = await getSession({ req: context.req });
+
+	if (session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {},
+	};
 }
