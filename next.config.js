966ddfa5c91c16052b08844ae3ee69e9ba5690eb
MAbdurahman <mdbdrrhm@gmail.com>
2021-08-06 04:18:21 -0500

    Modify: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	reactStrictMode: true,
 	env: {
-		MONGO_DB_URI: 'mongodb://localhost:27017/next-reserved',
-		SEED_DB_URI: 'mongodb://localhost:27017/next-reserved',
 
 		CLOUDINARY_CLOUD_NAME: 'mdbdrrhm',
 		CLOUDINARY_API_KEY: '485699669179417',
 		CLOUDINARY_API_SECRET: 'T6yk71envnm0oFbnWSjFt_PX2pQ',
+
+		MONGO_DB_URI: 'mongodb://localhost:27017/next-reserved',
+		SEED_DB_URI: 'mongodb://localhost:27017/next-reserved',
 
 		NEXTAUTH_URL: '',
 
