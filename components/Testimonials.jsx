fa48fedaf9854cc914147b8d9a2f08858c03e0e0
MAbdurahman <mdbdrrhm@gmail.com>
2021-08-20 23:38:26 -0500

    Complete TestimonialCarousel.jsx and Testimonials.jsx for desktop

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import TestimonialCarousel from './layout/TestimonialCarousel';
 
 export default function Testimonials() {
-   return (
-      <div className='text-center'>
-         <h1>Testimonials Page</h1>
-      </div>
-   )
+	return (
+		<div id='testimonials' className='testimonials'>
+			<h1>Testimonials</h1>
+			<div className='container'>
+				<div className='testimonials-content'>
+					<TestimonialCarousel />
+				</div>
+			</div>
+		</div>
+	);
 }
---
+++ components/layout/TestimonialCarousel.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+/* import testimonial_1 from '../../img/testimonials/testimonial-1.jpg';
+import testimonial_2 from '../../img/testimonials/testimonial-2.jpg';
+import testimonial_3 from '../../img/testimonials/testimonial-3.jpg';
+import testimonial_4 from '../../img/testimonials/testimonial-4.jpg';
+import testimonial_5 from '../../img/testimonials/testimonial-5.jpg'; */
+
+export default function TestimonialCarousel() {
+	return (
+		<Carousel
+			showArrows={false}
+			infiniteLoop={true}
+			showThumbs={false}
+			showStatus={false}
+			autoPlay={true}
+			interval={3000}
+		>
+			<>
+				<Image
+					src='images/testimonials/testimonial-1.jpg'
+					alt='Male Testimonial 1'
+				/>
+				<div className='myCarousel'>
+					<h3>Aaron Cuppari</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Debitis ratione sequi saepe dicta blanditiis, molestias eum
+						excepturi quod fugiat veniam!
+					</p>
+				</div>
+			</>
+			<>
+				<Image
+					src='images/testimonials/testimonial-2.jpg'
+					alt='Female Testimonial 2'
+				/>
+				<div className='myCarousel'>
+					<h3>Laura Spurrier</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Debitis ratione sequi saepe dicta blanditiis, molestias eum
+						excepturi quod fugiat veniam!
+					</p>
+				</div>
+			</>
+			<>
+				<Image
+					src='images/testimonials/testimonial-3.jpg'
+					alt='Male Testimonial 3'
+				/>
+				<div className='myCarousel'>
+					<h3>Esfir Novikov</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Debitis ratione sequi saepe dicta blanditiis, molestias eum
+						excepturi quod fugiat veniam!
+					</p>
+				</div>
+			</>
+			<>
+				<Image
+					src='images/testimonials/testimonial-4.jpg'
+					alt='Female Testimonial 4'
+				/>
+				<div className='myCarousel'>
+					<h3>Kaitlyn Devney</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Debitis ratione sequi saepe dicta blanditiis, molestias eum
+						excepturi quod fugiat veniam!
+					</p>
+				</div>
+			</>
+			<>
+				<Image
+					src='images/testimonials/testimonial-5.jpg'
+					alt='Male Testimonial 5'
+				/>
+				<div className='myCarousel'>
+					<h3>Gary Bauer</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Debitis ratione sequi saepe dicta blanditiis, molestias eum
+						excepturi quod fugiat veniam!
+					</p>
+				</div>
+			</>
+		</Carousel>
+	);
+}
